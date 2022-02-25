95c5b0b2cba00a9bb488bdc0dfc524d4ec3548d5
Nathan Landers <nlanders87@gmail.com>
2022-02-25 13:37:08 -0500

    Finished code

--- js/script.js
+++ js/script.js
@@ -39,21 +39,30 @@ const quotes = [
   }
 ];
 
-let html = document.getElementById('quote-box');
-let lastQuote;
-let emptyProp = "";
+let html = document.getElementById('quote-box'); //sets the html variable to use the getElementbyId method to select the quote-box element
+let emptyProp = ""; //variable that stores an empty string to be compared against in the printQuote() function
+
 
 /***
  *  Function that creates a random number between 0 and the length of the quotes array. It then returns a random object within the quotes array. 
 ***/
 
 function getRandomQuote() {
-    let randomQuote = Math.floor(Math.random() * quotes.length);
+    let randomQuote = Math.floor(Math.random() * quotes.length); 
     return quotes[randomQuote];
 }
 
+/* Function that creates a random color by generating a random number and multiplying it by the total number of possible hex values. 
+   It then converts it to a string. With .toString it will return letters of the alphabet for any number higher than 9. So in this case it wil return 0123456789abcdef.
+   Then it reduces the possible length to 6 and rounds it down to the nearest integer.  Then concatenates a '#' to the front to make it a readable hex value */
+
+function getRandomColor() {
+  let randomColor = '#' + Math.floor(Math.random() * (256* (256 * 256)).toString(16).padStart(6)); 
+  return randomColor;
+}
+
 /***
- * `printQuote` function
+ *  Function that calls the getRandomQuote() function and then uses its value to print html to the page
 ***/
 
 function printQuote() {
@@ -62,14 +71,10 @@ function printQuote() {
             <p class="quote">${firstQuote.quote}</p>
             <p class="source">${firstQuote.source}`;
       
-     /* if (firstQuote === lastQuote ) {
-        firstQuote = getRandomQuote();
-      } else {
-        lastQuote = getRandomQuote();
-      } */
+//Conditional statements to compare the citation, year and tag property values to an empty string. Then assigns a temperate literal to print to the page when it evaluates to true. 
 
-      if (quotes.citation !== emptyProp){
-       markup += `<span class = "citation">${firstQuote.citation}</span>`;
+      if (quotes.citation !== emptyProp){                                     
+       markup += `<span class = "citation">${firstQuote.citation}</span>`;     
       } 
 
       if (quotes.year !== emptyProp) {
@@ -77,15 +82,18 @@ function printQuote() {
       } 
 
       if (quotes.tag !== emptyProp) {
-      markup += `<span class = ""source">, ${firstQuote.tag}</span>`;
+       markup += `<span>, ${firstQuote.tag}</span>`;
       } 
-      markup += `</p>`;
+      markup += `</p>`; //adds the closing the paragraph tag to the html string
 
-      html.innerHTML = markup;
+      html.innerHTML = markup;  //sets the html variable to inherit the .innerhtml property and gives it the value from the previous conditional statements
+
+      document.body.style.background = getRandomColor(); // calls the random color function and sets it's value to the background element 
 }
 
-printQuote();
+printQuote(); //call printQuote() function 
 
+setInterval(printQuote, 8000); //sets a timer to refresh the quote every 8 seconds
 
 /***
  * click event listener for the print quote button
@@ -93,3 +101,5 @@ printQuote();
 ***/
 
  document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+ 
